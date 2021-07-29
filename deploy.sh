7806e759756356de1525773a2c5583389f580633
sandeshnaroju <sandeshnaroju@gmail.com>
2021-07-29 13:46:53 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ ETAG=$(echo $PUSHED | awk '{sub(/.*eTag=/,""); sub(/ --.*/,""); print;}')
 
 echo $ETAG
 
-DEPLOY=$(aws deploy create-deployment --application-name <codedeploy_app_name> --s3-location bucket=<bucket_name>,key=<project_name>-$1.zip,bundleType=zip,eTag=$ETAG --ignore-application-stop-failures --deployment-group-name <codedeploy_deployment_group_name> --deployment-config-name CodeDeployDefault.OneAtATime --description "$2")
+DEPLOY=$(aws deploy create-deployment --application-name <codedeploy_app_name> --s3-location bucket=<bucket_name>,key=<project_name>-$1.zip,bundleType=zip,eTag=$ETAG --ignore-application-stop-failures --deployment-group-name <codedeploy_group_name> --deployment-config-name CodeDeployDefault.OneAtATime --description "$2")
 wait
 
 echo $DEPLOY
